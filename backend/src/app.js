46721af813064633b1d79dc21bff43adc94d426e
Tanishkaa23 <tanishkagupta203@gmail.com>
2025-05-18 21:40:14 +0530

    cors updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,22 +1,51 @@
+
 const express = require('express');
 const app = express();
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
+const userRoutes = require('./routes/user.routes'); 
+const pollRoutes = require('./routes/poll.routes');   
+
+const allowedOrigins = [
+  'http://localhost:5173',       
+  'https://myvotemate.vercel.app' 
+];
+
 app.use(cors({
-  origin: [
-    'http://localhost:5173', 
-    'https://myvotemate.vercel.app'
-  ],
-  credentials: true
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error(`CORS policy does not allow access from origin: ${origin}`));
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin', 'Access-Control-Allow-Credentials', 'Access-Control-Allow-Origin'],
+  optionsSuccessStatus: 204,
+  preflightContinue: false  
 }));
 
-app.use(cookieParser())
-app.set('view engine', 'ejs')
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-const userRoutes = require('./routes/user.routes')
-const pollRoutes = require('./routes/poll.routes')
-app.use('/user',userRoutes)
-app.use('/api',pollRoutes)
-module.exports= app;
+
+
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.get('/', (req, res) => {
+  res.send('Voting App Backend is Up and Running!');
+});
+
+app.use('/user', userRoutes);
+app.use('/api', pollRoutes);
+
+app.use((err, req, res, next) => {
+  console.error("Unhandled error:", err.stack || err.message || err);
+  res.status(err.status || 500).json({
+    error: err.message || 'Something went wrong on the server!'
+  });
+});
+
+module.exports = app;
